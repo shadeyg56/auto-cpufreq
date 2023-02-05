ade1fed790f2ebbaae4e92ce8879ab812c9b2850
shadeyg56 <shadeyg56@gmail.com>
2023-02-05 15:57:17 -0600

    actually fix css

--- auto_cpufreq/gui/app.py
+++ auto_cpufreq/gui/app.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append("../")
 from auto_cpufreq.gui.objects import RadioButtonView, SystemStatsLabel, CPUFreqStatsLabel, CurrentGovernorBox
 
-CSS_FILE = "styles.css"
+CSS_FILE = "/usr/local/share/auto-cpufreq/scripts/style.css"
 
 HBOX_PADDING = 20
 
@@ -20,7 +20,7 @@ class MyWindow(Gtk.Window):
         self.set_default_size(640, 480)
         self.set_border_width(10)
 
-        #self.load_css()
+        self.load_css()
 
         settings = Gtk.Settings.get_default()
         # Theme
